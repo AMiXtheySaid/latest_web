dcf744e7ec7605063c67116dd1a0cd19dec70379
AMiXtheySaid <a.fenesan2003@gmail.com>
2023-11-29 12:01:32 +0200

    login_register_pages_done

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+const exp = require('constants');
 const express = require ('express');
 const path = require ('path')
 const app = express();
 const port = 2999;
 
+app.use(express.static(path.join(__dirname, '/frontend')))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, `./frontend/html/main_page.html`))
+app.get('/home', (req, res) => {
+    res.sendFile(path.resolve(__dirname, `frontend/main_page.html`))
 })
 
 app.listen(port, () =>
