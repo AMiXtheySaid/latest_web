2cfcca90e1d50d9defb412ddc78ae87b5052fc68
AMiXtheySaid <a.fenesan2003@gmail.com>
2024-05-28 11:49:14 +0300

    fixed the navbar in contacts

--- frontend/js/contact.js
+++ frontend/js/contact.js
@@ -64,6 +64,32 @@ document.addEventListener('DOMContentLoaded', async function() {
     }
 })
 
+document.getElementById('contact').onclick = function() {
+    window.location.replace('/contact');
+}
+
+document.getElementById('aboutUs').onclick = function() {
+    window.location.replace('/about-us');
+}
+
+document.getElementById('logOut').onclick = function() {
+    deleteCookie('myToken');
+    window.location.replace('/home');
+}
+
+document.getElementById('changePassword').onclick = function() {
+    window.location.replace('/change-password');
+}
+
+document.getElementById('deleteAccount').onclick = function() {
+    window.location.replace('/delete-account');
+}
+
+document.getElementById('makeAnAppointment').onclick = function() {
+    window.location.replace('/appointments');
+}
+
 document.getElementById('homeBtn').onclick = function() {
     window.location.replace('/home');
 }
+
